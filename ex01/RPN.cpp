74b936951abb640974471daf15e3d091a9bde844
el mehdi bouabbadi <ebouabbadi@gmail.com>
2023-07-19 09:49:04 +0100

    commit change

--- ex01/RPN.cpp
+++ ex01/RPN.cpp
@@ -10,12 +10,15 @@ RPN::RPN(char *av)
     this->_arg = av;
     this->parseRPN();
 }
+
 void RPN::parseRPN()
 {
+    if (this->_arg.empty())
+        throw "Error"; 
     for (size_t i = 0; i < this->_arg.size(); i++)
     {
         if (!isdigit(this->_arg[i]) && this->_arg[i] != '*'
-            && this->_arg[i] != '-' &&  this->_arg[i] != '+' 
+            && this->_arg[i] != '-' &&  this->_arg[i] != '+'
             && this->_arg[i] != '/' && this->_arg[i] != ' ')
                 throw "Error";
     }
