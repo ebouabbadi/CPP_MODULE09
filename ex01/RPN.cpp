7afbd06e46bf893b12778ea236872feaa2f9716d
el mehdi bouabbadi <ebouabbadi@gmail.com>
2023-07-22 09:28:15 +0100

    ex01

--- ex01/RPN.cpp
+++ ex01/RPN.cpp
@@ -14,23 +14,28 @@ RPN::RPN(char *av)
 void RPN::parseRPN()
 {
     if (this->_arg.empty())
-        throw "Error"; 
+        throw "Error";
     for (size_t i = 0; i < this->_arg.size(); i++)
     {
-        if (!isdigit(this->_arg[i]) && this->_arg[i] != '*'
-            && this->_arg[i] != '-' &&  this->_arg[i] != '+'
-            && this->_arg[i] != '/' && this->_arg[i] != ' ')
-                throw "Error";
+        if (!isdigit(this->_arg[i]) && this->_arg[i] != '*' && this->_arg[i] != '-' && this->_arg[i] != '+' && this->_arg[i] != '/' && this->_arg[i] != ' ')
+            throw "Error";
     }
 }
 
-void    RPN::workNow(std::string arg)
+void RPN::workNow(std::string arg)
 {
     for (size_t i = 0; i < arg.size(); i++)
     {
-        std::cout<<arg[i];
+        std::string nb;
+        while (arg[i] && isdigit(arg[i]))
+        {
+            nb.push_back(arg[i]);
+            i++;
+        }
+        this->_Rpn.push_back(std::atoi(nb.c_str()));
+        std::cout << nb << std::endl;
+        std::cout << "................." << std::endl;
     }
-    
 }
 
 RPN::~RPN()
